6dd0162cf62fdd6ea54215b9b1b72dad6c2f885e
eya gueddes <eyagueddes1@gmail.com>
2021-03-20 05:50:44 +0100

    four

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,13 +1,12 @@
-import uuid from'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
+
 export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
+  const id = uuidv4();
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id }
+  });
 
-
-    const id = uuid.v4();//to get random universal //VERSION4
-    dispatch({// create action
-      type: SET_ALERT,//to resolve the type
-      payload: { msg, alertType, id }//send a payload  
-    });
-  
-    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
-  };
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+};
