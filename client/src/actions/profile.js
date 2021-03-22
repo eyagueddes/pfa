2acf02ac5b4a785911b6ae1d5a4fa8a443c0198a
eya gueddes <eyagueddes1@gmail.com>
2021-03-22 01:42:07 +0100

    commit ad

---
+++ client/src/actions/profile.js
@@ -0,0 +1,32 @@
+import api from '../utils/api';
+import { setAlert } from './alert';
+import axios from 'axios';
+
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  CLEAR_PROFILE,
+  ACCOUNT_DELETED,
+  GET_REPOS,
+  NO_REPOS
+} from './types';
+
+// Get current users profile
+export const getCurrentProfile = () => async (dispatch) => {
+    try {
+      const res = await api.get('/profile/me');
+  
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+ 
